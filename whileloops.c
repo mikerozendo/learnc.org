9248ab26aabf21743dd99bef65182ff02e1d2f8f
mikerozendo <mikerozendo@gmail.com>
2023-12-06 13:00:00 -0300

    whileloops.c

---
+++ whileloops.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Link to exercise: https://www.learn-c.org/en/While_loops
+
+int main() {
+    int array[] = {1, 7, 4, 5, 9, 3, 5, 11, 6, 3, 4};
+    int i = 0;
+
+    while (i < 10) {
+
+        i++;
+        if(array[i] < 5) continue;
+        if(array[i] > 10) break;
+
+        printf("%d\n", array[i]);
+    }
+
+    return 0;
+}
